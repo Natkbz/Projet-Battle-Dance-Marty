20661848faec064db8d781189af042872533253f
Natkbz <n02knat@outlook.fr>
2026-05-19 08:54:49 +0200

    gestion des bras

--- Découverte de Marty/main.py
+++ Découverte de Marty/main.py
@@ -5,7 +5,7 @@ my_marty = Marty("wifi","192.168.0.101")
 #marche en avant
 #my_marty.walk(3,'auto',0,30,4000) 
 #se remet droit normal
-my_marty.stand_straight(100)
+my_marty.stand_straight(1000)
 #dance 
 #my_marty.dance('right',  3000)
 #yeux (emotion, temps de deplacement ms)
@@ -14,4 +14,7 @@ my_marty.stand_straight(100)
 #my_marty.eyes('wide',1000) #ecarté 
 #my_marty.eyes('wiggle',4000)#bouge partout
 #coup de pied ( le pied, l'angle de la cheville, le temps)
-my_marty.kick('right',0,2000)
+#my_marty.kick('right',0,2000)
+#les bras (angle bras gauche, angle bras droit, tmps dep)
+#de -80 à 140 (-80 bras en arrière, 140 en haut et 0 en bas le long du corps)
+#my_marty.arms(0,0,2000)
