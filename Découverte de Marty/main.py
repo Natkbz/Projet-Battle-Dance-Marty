aeaa5794a765e457d986d6f652bc486b0681a2a5
Natkbz <n02knat@outlook.fr>
2026-05-22 17:07:55 +0200

    Récupération des composantes RGB

--- Découverte de Marty/main.py
+++ Découverte de Marty/main.py
@@ -4,20 +4,12 @@ import time
 from MartyContext import MartyContext
 
 def main():
-    ip_robot = "192.168.0.109"  
+    ip_bot = "192.168.0.109"  
 
-    my_marty = MartyContext(ip_robot)
-
-    my_marty.move_arm('ALU')
-    my_marty.eyes_expression('wide')
-    
-
-# Point d'entrée du script
-if __name__ == "__main__":
-    main()
-
-""""
- 
+    #my_marty = MartyContext(ip_robot)
+    my_marty = Marty("wifi",ip_bot)
+    my_marty.stand_straight(500)
+    print("jaune",getColor(my_marty))
     
 def getColor(marty):
     colorR = marty.get_color_sensor_value_by_channel('left','red')
@@ -25,44 +17,7 @@ def getColor(marty):
     colorB = marty.get_color_sensor_value_by_channel('left','blue')
     return (colorR,colorG,colorB)
 
-def calibrageCouleur(marty,couleur):
-    print("placer la couleur ",couleur,"sous le marty")
-    input("Appuyez sur Entrée quand le pied est bien positionné...")
-    valeurR =[]
-    valeurG = []
-    valeurB = []
-    for i in range(10) :
-        color = getColor(marty)
-        valeurR.append(color[0])
-        valeurG.append(color[1])
-        valeurB.append(color[2])
-        time.sleep(1)
-    moyenneR = sum(valeurR)/10
-    moyenneG = sum(valeurG)/10
-    moyenneB = sum(valeurB)/10
-    print("calibrage finit")
-    return (moyenneR,moyenneG,moyenneB)
 
-def calibrageTotal(marty):
-    jaune = calibrageCouleur(marty,"jaune")
-    rose = calibrageCouleur(marty,"rose")
-    noir = calibrageCouleur(marty,"noir")
-    vert = calibrageCouleur(marty,"vert")
-    
-
-my_marty = connect("192.168.0.109")
-my_marty.stand_straight(500)
-#print(have_walk)
-#move_foot(my_marty,'U',3)
-#move_foot(my_marty,'L',1)
-#move_foot(my_marty,'B',2)
-#move_arm(my_marty,'ALU')
-jaune = calibrageCouleur(my_marty,"jaune")
-rose = calibrageCouleur(my_marty,"rose")
-noir = calibrageCouleur(my_marty,"noir")
-vert = calibrageCouleur(my_marty,"vert")
-print("jaune", jaune)
-print("rose",rose)
-print("noir",noir)
-print("vert",vert)
-"""
+# Point d'entrée du script
+if __name__ == "__main__":
+    main()
