0a8214a242855b8add029e7301c7b45b8fb948c7
dgarnaux <dimitri.garnaux@gmail.com>
2026-05-22 15:58:50 +0200

    création main.py + GET / + GET /score

---
+++ main.py
@@ -0,0 +1,46 @@
+import http.server
+
+PORT = 8000
+
+def recherche_robot(id):
+    for i in range(len(liste_robots)):
+        if(liste_robots[i].id == id):
+            return liste_robots[i]
+
+
+class Robot():
+    score_final = 0
+    id = 0
+    def setId(self, id):
+        self.id = id
+        
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        request = self.requestline
+        list = request.split(" ")
+        
+        if (list[1] == "/"):
+            self.send_response(200)
+            self.send_header("Content-type", "text")
+            self.end_headers()
+            self.wfile.write(bytes(self.server_version))
+        elif(list[1] == "/score"):
+            self.send_response(200)
+            self.send_header("Content-type", "text")
+            self.end_headers()
+            
+            self.wfile.write(bytes(recherche_robot(list[3]).score_final))
+
+server = http.server.HTTPServer(("127.0.0.1", PORT), Handler)
+
+
+liste_robots = []
+robot_1 = Robot()
+liste_robots.append(robot_1)
+
+
+def run() :
+    print("serving at port", PORT)
+    server.serve_forever()
+
+run()
