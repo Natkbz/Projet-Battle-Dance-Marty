554a34e1449407f5e2b7230becf8df6665e48399
dgarnaux <dimitri.garnaux@gmail.com>
2026-05-22 16:41:57 +0200

    POST /strat

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import http.server
 
 PORT = 8000
 
+nombreDePasBattle = 10    ########TEMPORAIRE#########
+
 def recherche_robot(id):
     for i in range(len(liste_robots)):
         if(liste_robots[i].id == id):
@@ -11,8 +13,12 @@ def recherche_robot(id):
 class Robot():
     score_final = 0
     id = 0
+    nombreDePasRestants = 0
+    
     def setId(self, id):
         self.id = id
+    def setNbrDePasRestants(self, nbr):
+        self.nombreDePasRestants = nbr
         
 class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -23,13 +29,14 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text")
             self.end_headers()
-            self.wfile.write(bytes(self.server_version))
+            self.wfile.write(bytes(self.server_version.encode()))
         elif(list[1] == "/score"):
             self.send_response(200)
             self.send_header("Content-type", "text")
             self.end_headers()
             
-            self.wfile.write(bytes(recherche_robot(list[2]).score_final))
+            idRequeset = self.rfile.readline()
+            self.wfile.write(bytes(recherche_robot(idRequeset).score_final))
             
             
     def do_POST(self):
@@ -47,7 +54,19 @@ class Handler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "text")
             self.end_headers()
             
-            self.wfile.write(bytes(recherche_robot(list[2]).score_final))
+            self.wfile.write(bytes(newId))
+            
+        elif(list[1] == "/start"):
+            idRequeset = self.rfile.readline()
+            
+            recherche_robot(idRequeset).setNbrDePasRestants(nombreDePasBattle)
+            
+            self.send_response(200)
+            self.send_header("Content-type", "text")
+            self.end_headers()
+            
+            self.wfile.write(bytes(nombreDePasBattle))
+            
             
             
             
