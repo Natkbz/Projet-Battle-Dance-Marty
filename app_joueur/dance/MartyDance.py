60e63410739fece98683a8033323e79989ae7e1f
Natkbz <n02knat@outlook.fr>
2026-06-03 10:58:39 +0200

    feat:mouvement et dance du robot

--- app_joueur/dance/MartyDance.py
+++ app_joueur/dance/MartyDance.py
@@ -6,4 +6,26 @@ class MartyDance:
         self.file_dance = FileDance(path)
         self.move = self.file_dance.getMvt()
         self.colorDance = self.file_dance.getColorDance()
+        print(self.move)
+        print(self.colorDance)
 
+    def dance(self):
+        for i in range(6):
+            print("mouvement ",i)
+            self.execute_mouvement(i)
+            self.marty.normal(1500)
+            self.check_color()
+            
+            
+    def execute_mouvement(self,n_move):
+        self.marty.move_foot(self.move[n_move][0], int(self.move[n_move][1]))
+        
+    def check_color(self):
+        color = self.marty.getColor()
+        print(color)
+        if color in self.colorDance:
+            bras = self.colorDance[color][0]
+            expressionToDo = self.colorDance[color][1]
+            for i in range(len(bras)):
+                self.marty.move_arm(bras[i])
+            self.marty.expression(expressionToDo)
