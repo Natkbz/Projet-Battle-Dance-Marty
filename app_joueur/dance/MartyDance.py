2c86323e449a6fb4f1691d868ddec2eeafad8dae
dcr29 <dcr.-@outlook.com>
2026-06-17 15:38:10 +0200

    fix: correction bug newdance

--- app_joueur/dance/MartyDance.py
+++ app_joueur/dance/MartyDance.py
@@ -65,5 +65,5 @@ class MartyDance:
         self.file_dance = FileDance(path)
         self.move = self.file_dance.getMvt()
         self.colorDance = self.file_dance.getColorDance()
-        self.dance()
+        
         
